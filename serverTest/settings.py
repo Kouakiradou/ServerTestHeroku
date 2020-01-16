efcfa974acdafad71fd7f1a854322aa7e30fdb85
江晟同 <kouakiradou@jiangshengtongdeMacBook-Pro.local>
2020-01-16 21:08:38 +0000

    2

--- serverTest/settings.py
+++ serverTest/settings.py
@@ -131,6 +131,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
